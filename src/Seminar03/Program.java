02e9fa3f0df1013db6bc59f9e2cff42a08c81475
SergeySkokov <skokovs@inbox.ru>
2022-11-02 20:50:18 +0400

    "Added Wave Alg"

---
+++ src/Seminar03/Program.java
@@ -0,0 +1,75 @@
+package Seminar03;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import javax.sql.rowset.spi.XmlReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Program {
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+
+        int x = 0;
+        int y = 0;
+        int nx = 0;
+        int ny = 0;
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document xDoc = db.parse(XmlReader.class.getResourceAsStream("/data.xml"));
+        xDoc.normalize();
+        NodeList nList = xDoc.getElementsByTagName("Size");
+
+        Node node = nList.item(0);
+        Element element = (Element) node;
+
+        int height = Integer.parseInt(element.getElementsByTagName("Height").item(0).getTextContent());
+        int width = Integer.parseInt(element.getElementsByTagName("Width").item(0).getTextContent());
+        WaveAlg Path = new WaveAlg(width, height);
+
+        nList = xDoc.getElementsByTagName("Block");
+        for (int i = 0; i < nList.getLength(); i++) {
+            node = nList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                element = (Element) node;
+                x = Integer.parseInt(element.getElementsByTagName("X").item(0).getTextContent());
+                y = Integer.parseInt(element.getElementsByTagName("Y").item(0).getTextContent());
+                Path.block(x, y);
+            }
+        }
+
+        Path.traceOut();
+        System.out.println();
+
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.println("??????? x ??????");
+            x = scan.nextInt();
+            System.out.println("??????? y ??????");
+            y = scan.nextInt();
+            System.out.println("??????? x ?????");
+            nx = scan.nextInt();
+            System.out.println("??????? y ?????");
+            ny = scan.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("??????? ?????!");
+        }
+
+        Path.findPath(x, y, nx, ny);
+
+        Path.traceOut();
+        System.out.println();
+
+        Path.waveOut();
+    }
+
+}
